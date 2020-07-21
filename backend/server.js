1245403e0a55bab6d0a184c6f49ca84c90e90bf3
lauris-kuznecovs <lauris.kuznecovs@wunder.io>
2020-07-21 12:03:01 +0300

    Updating Backend dependencies to latest.

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,8 @@ const router = require('./router');
 const app = express();
 mongoose.connect(db, {
   useNewUrlParser: true,
-  useCreateIndex: true
+  useCreateIndex: true,
+  useUnifiedTopology: true,
 });
 
 app.use(morgan('combined'));
